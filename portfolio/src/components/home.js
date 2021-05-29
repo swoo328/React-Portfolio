74e2d4c28185d169e6b7ecccbcd1a9e3f9c044b9
Steven Woo <swoo9875@gmail.com>
2021-05-29 11:24:15 -0400

    adding more styling on the portfolio side

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import ReactTypingEffect from 'react-typing-effect';
+// import ReactTypingEffect from 'react-typing-effect';
+import Typewriter from "typewriter-effect";
 
 function home (){
     return(
       // add more features and words
-      <div class = "typing">
-        <ReactTypingEffect
-          text = "Hi, I'm Steven Woo"
-        />  
-      </div>  
+      <div className = "intro">
+      <Typewriter
+      // delays and looping
+      options = {{
+          loop:true,
+          delay: 100
+      }}
+      onInit = {(typewriter) => {
+       
+          typewriter
+              .typeString("Hey, I'm Steven")
+              .pauseFor()
+              // .typeString("")
+              // .deleteAll()
+              // .typeString("Come Join Us On Our Adventure")
+              // .deleteAll()
+              // .typeString("To Find The Perfect Vacation With Us")
+              // .deleteAll()
+              .start();
+      }}
+      />
+      </div>
     )  
 }
 
